2db94b5875503c9f3aa625394ea3ae94e012ca9f
Wato-Adano <watobuchaadano@gmail.com>
2024-03-22 16:31:54 +0300

    add file

---
+++ arrays.js
@@ -0,0 +1,35 @@
+//Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true, "green", "where",12,56]=
+let arr1=[3,7,34,90,12];
+console.log('last item', arr1[arr1.length -1]);
+let slicearr1 = arr1.slice(-1);
+console.log({arr1});
+
+let arr2=[true,"green", "where" ,12,56];
+console.log('last item', arr2[arr2.length -1]);
+let slicearr2 =arr2.slice(-1);
+console.log({arr2})
+
+
+//Write a JS program that will join the following array elements into a stringmyPets = ["Cow", "Bird", "Snake", "Dog"];
+const myPets = ["Cow","Bird","Snake", "Dog"];
+let newPets = myPets.toString();
+console.log({newPets});
+
+
+//Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+
+const number = [-5,9,5,3,2,-3,6,8,4,1];
+number.sort();
+console.log({number})
+
+//Write a program to remove duplicates from the following array.Console the array without duplicates, and console another array that only contains the duplicates
+let arr = ["boy", "man", "girl",            "school", "girl", "woman"];
+
+
+//Write a JS script to search for the following word in the array."food"If the word is present, return it else return "the search word was not found"let arr5 = ["the", "way", "x", 4];
+let arr5=["the","way","x",4]
+
+//Write a JS script to sort the following string:let word = "renniw"
+const word =["renniw"];
+word.sort();
+console.log({word});
